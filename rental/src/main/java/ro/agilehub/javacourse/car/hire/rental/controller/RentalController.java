d939a5357fdf30895b6ab5f6fde973762b00d1a3
viorel.smeu <viorel.smeu@eos-it.ro>
2020-11-21 08:38:10 +0200

    Added api and return values for User/UserController

--- rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/controller/RentalController.java
+++ rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/controller/RentalController.java
@@ -2,7 +2,9 @@ package ro.agilehub.javacourse.car.hire.rental.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import ro.agilehub.javacourse.car.hire.api.model.CarDTO;
 import ro.agilehub.javacourse.car.hire.api.model.RentalDTO;
+import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
 import ro.agilehub.javacourse.car.hire.api.specification.RentalApi;
 
 import javax.validation.Valid;
@@ -12,11 +14,28 @@ import java.util.List;
 @RestController
 public class RentalController implements RentalApi {
 
+    private static final CarDTO car;
+    private static final UserDTO user;
+
+    static {
+        car = new CarDTO();
+        car.setMake("BMW");
+        car.setFuel("Gasoline");
+        car.setId(1);
+
+        user = new UserDTO();
+        user.setId(13);
+        user.setUserName("Viocartman");
+        user.setFirstName("Vio");
+        user.setLastName("Cartman");
+    }
+
     @Override
     public ResponseEntity<RentalDTO> createRental(@Valid RentalDTO rentalDTO) {
+
         RentalDTO rental = new RentalDTO();
-        rental.setCarId(1);
-        rental.setUserId(1);
+        rental.setCar(RentalController.car);
+        rental.setUser(RentalController.user);
         rental.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental.setStartDateTime(OffsetDateTime.now());
 
@@ -31,8 +50,8 @@ public class RentalController implements RentalApi {
     @Override
     public ResponseEntity<RentalDTO> getRental(Integer rentalId) {
         RentalDTO rental = new RentalDTO();
-        rental.setCarId(1);
-        rental.setUserId(1);
+        rental.setCar(car);
+        rental.setUser(user);
         rental.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental.setStartDateTime(OffsetDateTime.now());
 
@@ -42,14 +61,14 @@ public class RentalController implements RentalApi {
     @Override
     public ResponseEntity<List<RentalDTO>> getRentals(@Valid Integer userId, @Valid Integer carId, @Valid String status) {
         RentalDTO rental1 = new RentalDTO();
-        rental1.setCarId(1);
-        rental1.setUserId(1);
+        rental1.setCar(car);
+        rental1.setUser(user);
         rental1.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental1.setStartDateTime(OffsetDateTime.now());
 
         RentalDTO rental2 = new RentalDTO();
-        rental2.setCarId(2);
-        rental2.setUserId(2);
+        rental2.setCar(car);
+        rental2.setUser(user);
         rental2.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental2.setStartDateTime(OffsetDateTime.now());
 
@@ -59,8 +78,8 @@ public class RentalController implements RentalApi {
     @Override
     public ResponseEntity<RentalDTO> patchRental(Integer rentalId, @Valid RentalDTO rentalDTO) {
         RentalDTO rental1 = new RentalDTO();
-        rental1.setCarId(1);
-        rental1.setUserId(1);
+        rental1.setCar(car);
+        rental1.setUser(user);
         rental1.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental1.setStartDateTime(OffsetDateTime.now());
 
---
+++ user.management/src/main/java/ro/agilehub/javacourse/car/hire/user/controller/UserController.java
@@ -0,0 +1,68 @@
+package ro.agilehub.javacourse.car.hire.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
+import ro.agilehub.javacourse.car.hire.api.specification.UserApi;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class UserController implements UserApi {
+
+    @Override
+    public ResponseEntity<UserDTO> createUser(@Valid UserDTO userDTO) {
+        UserDTO user = new UserDTO();
+        user.setId(13);
+        user.setUserName("Viocartman");
+        user.setFirstName("Vio");
+        user.setLastName("Cartman");
+
+        return ResponseEntity.ok(user);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteUser(Integer userId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public ResponseEntity<UserDTO> getUser(Integer userId) {
+        UserDTO user = new UserDTO();
+        user.setId(13);
+        user.setUserName("Viocartman");
+        user.setFirstName("Vio");
+        user.setLastName("Cartman");
+
+        return ResponseEntity.ok(user);
+    }
+
+    @Override
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        UserDTO user1 = new UserDTO();
+        user1.setId(13);
+        user1.setUserName("Viocartman");
+        user1.setFirstName("Vio");
+        user1.setLastName("Cartman");
+
+        UserDTO user2 = new UserDTO();
+        user2.setId(26);
+        user2.setUserName("Viocartman1");
+        user2.setFirstName("Vio1");
+        user2.setLastName("Cartman1");
+
+        return ResponseEntity.ok(List.of(user1, user2));
+    }
+
+    @Override
+    public ResponseEntity<UserDTO> patchUser(Integer userId, @Valid UserDTO userDTO) {
+        UserDTO user2 = new UserDTO();
+        user2.setId(26);
+        user2.setUserName("Viocartman1");
+        user2.setFirstName("Vio1");
+        user2.setLastName("Cartman1");
+
+        return ResponseEntity.ok(user2);
+    }
+}
