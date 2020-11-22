07d9160787b5e70e1985d1417db6f88099c70bce
viorel.smeu <viorel.smeu@eos-it.ro>
2020-11-22 07:36:40 +0200

    Refactored RentalDTO to use ids for car and user

--- rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/controller/RentalController.java
+++ rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/controller/RentalController.java
@@ -2,9 +2,7 @@ package ro.agilehub.javacourse.car.hire.rental.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import ro.agilehub.javacourse.car.hire.api.model.CarDTO;
 import ro.agilehub.javacourse.car.hire.api.model.RentalDTO;
-import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
 import ro.agilehub.javacourse.car.hire.api.specification.RentalApi;
 
 import javax.validation.Valid;
@@ -14,28 +12,12 @@ import java.util.List;
 @RestController
 public class RentalController implements RentalApi {
 
-    private static final CarDTO car;
-    private static final UserDTO user;
-
-    static {
-        car = new CarDTO();
-        car.setMake("BMW");
-        car.setFuel("Gasoline");
-        car.setId(1);
-
-        user = new UserDTO();
-        user.setId(13);
-        user.setUserName("Viocartman");
-        user.setFirstName("Vio");
-        user.setLastName("Cartman");
-    }
-
     @Override
     public ResponseEntity<RentalDTO> createRental(@Valid RentalDTO rentalDTO) {
 
         RentalDTO rental = new RentalDTO();
-        rental.setCar(RentalController.car);
-        rental.setUser(RentalController.user);
+        rental.setCarId(1);
+        rental.setUserId(1);
         rental.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental.setStartDateTime(OffsetDateTime.now());
 
@@ -50,8 +32,8 @@ public class RentalController implements RentalApi {
     @Override
     public ResponseEntity<RentalDTO> getRental(Integer rentalId) {
         RentalDTO rental = new RentalDTO();
-        rental.setCar(car);
-        rental.setUser(user);
+        rental.setCarId(1);
+        rental.setUserId(2);
         rental.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental.setStartDateTime(OffsetDateTime.now());
 
@@ -61,14 +43,14 @@ public class RentalController implements RentalApi {
     @Override
     public ResponseEntity<List<RentalDTO>> getRentals(@Valid Integer userId, @Valid Integer carId, @Valid String status) {
         RentalDTO rental1 = new RentalDTO();
-        rental1.setCar(car);
-        rental1.setUser(user);
+        rental1.setCarId(1);
+        rental1.setUserId(2);
         rental1.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental1.setStartDateTime(OffsetDateTime.now());
 
         RentalDTO rental2 = new RentalDTO();
-        rental2.setCar(car);
-        rental2.setUser(user);
+        rental2.setCarId(2);
+        rental2.setUserId(1);
         rental2.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental2.setStartDateTime(OffsetDateTime.now());
 
@@ -78,8 +60,8 @@ public class RentalController implements RentalApi {
     @Override
     public ResponseEntity<RentalDTO> patchRental(Integer rentalId, @Valid RentalDTO rentalDTO) {
         RentalDTO rental1 = new RentalDTO();
-        rental1.setCar(car);
-        rental1.setUser(user);
+        rental1.setCarId(1);
+        rental1.setUserId(1);
         rental1.setStatus(RentalDTO.StatusEnum.ACTIVE);
         rental1.setStartDateTime(OffsetDateTime.now());
 
